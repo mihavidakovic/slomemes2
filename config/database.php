2e3c16b5bc466feaef363d74f7137fb938da8c47
Miha Vidakovič <miha@vidakovic.si>
2017-03-21 21:42:21 +0100

    keke

--- config/database.php
+++ config/database.php
@@ -42,10 +42,10 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '8888'),
+            'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'slomemes'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'root'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', 'kOPwNBD3ruomc1JTi5Wf'),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',

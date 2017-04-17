8315052714e7255423ebc6f76937a099bb1f7fe2
Josip Rajković <sirsir55@gmail.com>
2017-04-17 19:21:39 +0200

    TIIIIIIIII MARKO ZDRAVOOOOO RATAL MAL MI JEEEE

--- public/js/index.js
+++ public/js/index.js
@@ -5,117 +5,69 @@ function prikaz(JSONposti) {
 	const slika = document.getElementsByClassName('slika')[0];
 	const like = document.getElementsByClassName('like')[0];
 	const downvote = document.getElementsByClassName('dislike')[0];
-	const ul = document.getElementsByClassName('comments')[0];
-	console.log("Začetek kode!");
+	const ul = document.getElementsByClassName('comments')[0];	
 
-	var zbirkaPregledanih = [];
-	var zbirka = [];
 	zbirka = JSONposti;
-	
-	//začetna pozicija v json zbirki, ki je naključna vrednost v našem primeru 0->20
-	var indeks = Math.floor(Math.random() * zbirka.length);
+	prikazPostov();
 
-	//Začetna pozicija indeksa in prikaz slike (stvar spremembe)
-	naslov.innerHTML = naslov.innerHTML + zbirka[indeks].title; //nastavimo tekst na vrednost polja v json zbirki z trenutnim indeksom
-	ustvarjeno.innerHTML = "Ustvarjeno: " + zbirka[indeks].created_at + " " + zbirka[indeks].id;
-	slika.style.backgroundImage = "url(" + zbirka[indeks].url + ")";
-	$.ajax({
-	    type:"GET", 
-	    url: "/komentarji/" + zbirka[indeks].id, 
-	    success: function(JSONkomentarji) {
-	           komentarji(JSONkomentarji.komentarji);
-	        },
-	   dataType: "jsonp"
-	});		
-
-
-	function komentarji(JSONkomentarji) {
-		ul.innerHTML = "";
-		for (var i = 0; i < JSONkomentarji.length; i++) {
-			var li = document.createElement('li');
-
-			ul.appendChild(li);
-			li.innerHTML = JSONkomentarji[i].content;
-		}
+	//bolje še enkrat nafilati zbirko naknadno, kot pa od začetka
+	function nafilajZbirko() {
+			zbirkaPregledanih = [];
+ 			zbirka = [];
+			$.ajax({
+		    type:"GET", 
+		    url: "/posti", 
+		    success: function(JSONposti) {		    		
+		            prikaz1(JSONposti.posti);
+		        },
+		   dataType: "jsonp"
+		});
 	}
-	zbirka.splice(indeks, 1);
-	zbirkaPregledanih.push(indeks);
-	console.log("Dolžina zbirke1: " + zbirka.length);
-	console.log("Indeks: " + indeks);	
-	console.log("Dolžina pregledanih:" + zbirkaPregledanih.length);	
-	//FUNKCIJE
-	//funkcija za prestavljanje
+	//vezano na zgornji komentar
+	function prikaz1(JSONposti) {
+		zbirka = JSONposti;		
+		prikazPostov();
+	}
+
+	//osnovna funkcija prikaza postov
+	function prikazPostov() {
+		var indeks = Math.floor(Math.random() * zbirka.length); //indeks se bo določal le v velikosti tabele zbirka, kar je odličen način omejevanja
+		naslov.innerHTML = ""; //ponastavimo tekst saj se v nasprotnem primeru le seštevajo od prej stringi
+		ustvarjeno.innerHTML = ""; //ponastavimo tekst saj se v nasprotnem primeru le seštevajo od prej stringi
+		naslov.innerHTML = naslov.innerHTML + zbirka[indeks].title; //nastavimo tekst na vrednost polja v json zbirki z trenutnim indeksom
+		ustvarjeno.innerHTML = "Ustvarjeno: " + zbirka[indeks].created_at + " " + zbirka[indeks].id;
+		slika.style.backgroundImage = "url(" + zbirka[indeks].url + ")"; //nastavimo sliko
+		/*V tem kosu kode pridobimo komentarje za vsak post*/
+		$.ajax({
+		    type:"GET", 
+		    url: "/komentarji/" + zbirka[indeks].id, 
+		    success: function(JSONkomentarji) {
+		           komentarji(JSONkomentarji.komentarji);
+		        },
+		   dataType: "jsonp"
+		});	
+		function komentarji(JSONkomentarji) {
+			ul.innerHTML = "";
+			for (var i = 0; i < JSONkomentarji.length; i++) {
+				var li = document.createElement('li');
+
+				ul.appendChild(li);
+				li.innerHTML = JSONkomentarji[i].content;
+			}
+		}
+		zbirka.splice(indeks, 1); //iz zbirke odstranimo ogledan post
+	}
+
+	//Funkcija naprej se sproži ko likamo ali dislikamo post
 	function naprej() {
-		if (zbirka.length == 1) {
-			pridobiPoste();
-			zbirkaPregledanih = [];			
-			console.log("Dolžina zbirke2: " + zbirka.length);
-			console.log("Indeks: " + indeks);
-			console.log("Dolžina pregledanih:" + zbirkaPregledanih.length);			
+		if (zbirka.length == 0) { //ko pregledamo zbirko
+			nafilajZbirko();
 		} else {
-			if(indeks > zbirka.length - 1) { //tukaj omejimo delovanje funkcije na velikost jsona v našem primeru 20
-			indeks = Math.floor(Math.random() * zbirka.length); //random 0->velikost zbirke
-			naslov.innerHTML = ""; //ponastavimo tekst na prazen string, če ne nam jih sešteva
-			ustvarjeno.innerHTML = "";
-			naslov.innerHTML = naslov.innerHTML + zbirka[indeks].title; //nastavimo tekst na vrednost polja v json zbirki z trenutnim indeksom
-			ustvarjeno.innerHTML = "Ustvarjeno: " + zbirka[indeks].created_at + " " + zbirka[indeks].id;
-			slika.style.backgroundImage = "url(" + zbirka[indeks].url + ")";			
-			zbirkaPregledanih.push(indeks);
-			function komentarji(JSONkomentarji) {
-				ul.innerHTML = "";
-				for (var i = 0; i < JSONkomentarji.length; i++) {
-					var li = document.createElement('li');
-
-					ul.appendChild(li);
-					li.innerHTML = JSONkomentarji[i].content;
-				}
+				prikazPostov();
 			}
-			$.ajax({
-			    type:"GET", 
-			    url: "/komentarji/" + zbirka[indeks].id, 
-			    success: function(JSONkomentarji) {
-			           komentarji(JSONkomentarji.komentarji);
-			        },
-			   dataType: "jsonp"
-			});
-			zbirka.splice(indeks, 1);
-			console.log("Dolžina zbirke3: " + zbirka.length);
-			console.log("Indeks: " + indeks);
-			console.log("Dolžina pregledanih:" + zbirkaPregledanih.length);	
-		} else {
-			indeks = Math.floor(Math.random() * zbirka.length); //random 0->velikost zbirke
-			naslov.innerHTML = "";
-			ustvarjeno.innerHTML = "";
-			naslov.innerHTML = naslov.innerHTML + zbirka[indeks].title;
-			ustvarjeno.innerHTML = "Ustvarjeno: " + zbirka[indeks].created_at + " " + zbirka[indeks].id;
-			slika.style.backgroundImage = "url(" + zbirka[indeks].url + ")";			
-			zbirkaPregledanih.push(indeks);
-			function komentarji(JSONkomentarji) {
-					ul.innerHTML = "";
-				for (var i = 0; i < JSONkomentarji.length; i++) {
-					var li = document.createElement('li');
-
-					ul.appendChild(li);
-					li.innerHTML = JSONkomentarji[i].content;
-				}
-			}
-			$.ajax({
-			    type:"GET", 
-			    url: "/komentarji/" + zbirka[indeks].id, 
-			    success: function(JSONkomentarji) {
-			           komentarji(JSONkomentarji.komentarji);
-			        },
-			   dataType: "jsonp"
-			});
-			zbirka.splice(indeks, 1);
-			console.log("Dolžina zbirke4: " + zbirka.length);
-			console.log("Indeks: " + indeks);
-			console.log("Dolžina pregledanih:" + zbirkaPregledanih.length);				
-
 		}
-	}
 
-}
+
 
 	//Event listenerji
 	like.addEventListener('click', naprej);
@@ -123,12 +75,16 @@ function prikaz(JSONposti) {
 
 }
 
+//Začetek kode, kjer pridobimo z linka vse poste v json obliki
 function pridobiPoste() {
+	zbirkaPregledanih = [];
+ 	zbirka = [];
 	$.ajax({
 		    type:"GET", 
 		    url: "/posti", 
-		    success: function(JSONposti) {
+		    success: function(JSONposti) {		    		
 		            prikaz(JSONposti.posti);
+
 		        },
 		   dataType: "jsonp"
 		});
